5cefd027529e84941540c21906be46ac2ae3e1af
blxtok <89456941+blxtok@users.noreply.github.com>
2022-04-19 17:58:04 +0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,7 @@
+local Game = ""
 
+if game.PlaceId == "2629642516" then
+   Game = "BitcoinMiner"
+end
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/blxtok/QuadX/main/games/" .. Game .. ".lua")()
