f14089de2d629fe3183375fc2f040c968075f35b
blxtok <89456941+blxtok@users.noreply.github.com>
2022-04-19 18:06:40 +0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -7,7 +7,7 @@ if game.PlaceId == "2629642516" then
 end
 
 if Supported == true then
-   loadstring(game:HttpGet('https://raw.githubusercontent.com/blxtok/QuadX/main/games/' .. Game .. '.lua')()
+   loadstring(game:HttpGet('https://raw.githubusercontent.com/blxtok/QuadX/main/games/' .. Game .. '.lua'))()
 else
    if syn then
       printconsole("Game not supported.")
