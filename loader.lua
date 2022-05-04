b5aea09d7d80ee9e2198f72e5de864a217510136
blxtok <89456941+blxtok@users.noreply.github.com>
2022-05-04 09:53:56 +0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,17 @@
-loadstring(game:HttpGet("https://quadx.gq/loader.lua")()
+local Game = nil
+local Supported = nil
+
+if game.PlaceId == 2629642516 then
+   Game = "BitcoinMiner"
+   Supported = true
+end
+
+if Supported == true then
+   loadstring(game:HttpGet('https://raw.githubusercontent.com/blxtok/QuadX/main/games/' .. Game .. '.lua'))()
+else
+   if syn then
+      printconsole("Game not supported.")
+   else
+      print("Game not supported.")
+   end
+end
